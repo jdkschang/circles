b6148758517f09790db0f10e5bedb313c018b560
Jonathan D. Chang <jdkschang@apple.com>
2024-05-18 12:53:40 -0700

    📦✨ Add `quil` library
    - 🔧♻️ Update project description

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject circles "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :description "A simple Clojure project for animated circle visualizations"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [quil "4.3.1563"]]
   :main ^:skip-aot circles.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
