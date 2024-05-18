320976e087a7be3ce7f183de7d99702bf8742dd5
Jonathan D. Chang <jdkschang@apple.com>
2024-05-18 13:25:52 -0700

    🧱✨ Add `setup`, `update`, & `draw`

--- src/circles/core.clj
+++ src/circles/core.clj
@@ -1,7 +1,36 @@
 (ns circles.core
+  (:require [quil.core :as q :include-macros true]
+            [quil.middleware :as m])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn setup []
+  (q/frame-rate 30)
+  {:circles (mapv (fn [_]
+                    {:x (q/random (q/width))
+                     :y (q/random (q/height))
+                     :size (q/random 10 50)
+                     :color [(q/random 255) (q/random 255) (q/random 255)]})
+                  (range 10))})
+
+(defn update-state [state]
+  (update state :circles (fn [circles]
+                           (mapv (fn [circle]
+                                   (assoc circle
+                                          :size (+ (:size circle) (q/random -1 1))))
+                                 circles))))
+
+(defn draw-state [state]
+  (q/background 255)
+  (doseq [circle (:circles state)]
+    (q/fill (apply q/color (:color circle)))
+    (q/ellipse (:x circle) (:y circle) (:size circle) (:size circle))))
+
+(defn -main [& args]
+  (q/defsketch circle-visualization
+    :title "Circles"
+    :host "localhost"
+    :setup setup
+    :update update-state
+    :draw draw-state
+    :size [800 600]
+    :middleware [m/fun-mode]))
